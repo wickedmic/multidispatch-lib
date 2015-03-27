5ec18c82adb45a13f75292555f72c7609526fb1e
Michael T. Becker <programming@wickedmic.com>
2015-03-27 09:05:57 +0100

    added foldr and foldl

---
+++ foldl.hpp
@@ -0,0 +1,28 @@
+#ifndef INCLGUARD_foldl_hpp
+#define INCLGUARD_foldl_hpp
+
+template<template<typename,typename> class Functor, typename InitValue, typename List>
+struct foldl;
+
+template<template<typename,typename> class Functor, typename InitValue, template<typename...> class List, typename Head, typename... Tail>
+struct foldl<Functor, InitValue, List<Head, Tail...>>
+{
+	using type =
+		typename foldl<
+			Functor,
+			typename Functor<
+				InitValue,
+				Head
+			>::type,
+			List<Tail...>
+		>::type;
+};
+
+template<template<typename,typename> class Functor, typename InitValue, template<typename...> class List>
+struct foldl<Functor, InitValue, List<>>
+{
+	using type = InitValue;
+};
+
+#endif
+
