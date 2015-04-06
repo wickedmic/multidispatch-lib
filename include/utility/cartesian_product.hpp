a9ef8545d98dd612479144469817b3930b4e8a1f
Michael T. Becker <programming@wickedmic.com>
2015-04-06 16:49:27 +0200

    cartesian_product redone + tests

--- include/utility/cartesian_product.hpp
+++ include/utility/cartesian_product.hpp
@@ -1,66 +1,16 @@
 #ifndef INCLGUARD_cartesian_product_hpp
 #define INCLGUARD_cartesian_product_hpp
 
+#include "map.hpp"
+#include "prepend.hpp"
+#include "foldr.hpp"
+#include "concat.hpp"
+
 namespace meta
 {
-	/// prepends Type to each list in List
-	template<typename Item, typename List>
-	struct foreach_prepend
-	{
-		template<typename _List>
-		struct prepend
-		{
-			using type = meta::prepend_t<Item, _List>;
-		};
-
-		using type = meta::map_t<prepend, List>;
-	};
-
-
-	/// if_
-	template<bool, typename TrueType, typename FalseType>
-	struct if_
-	{
-		using type = FalseType;
-	};
-
-
-	template<typename TrueType, typename FalseType>
-	struct if_<true, TrueType, FalseType>
-	{
-		using type = TrueType;
-	};
-
 
 	/// cartesian_product
 	template<typename... List> struct cartesian_product;
-
-	template<template<typename...> class List, typename Type, typename... Types, typename... OtherLists>
-	struct cartesian_product<List<Type, Types...>, OtherLists...>
-	{
-		using type =
-			typename meta::concat<
-				typename foreach_prepend<
-					Type,
-					typename if_<
-						sizeof...(OtherLists) != 0,
-						typename cartesian_product<OtherLists...>::type,
-						List<List<>>
-					>::type
-				>::type,
-				typename cartesian_product<
-					List<Types...>,
-					OtherLists...
-				>::type
-			>::type;
-	};
-
-	template<template<typename...> class List, typename... OtherLists>
-	struct cartesian_product<List<>, OtherLists...>
-	{
-		//using type = typename cartesian_product<OtherLists...>::type;
-		using type = List<>;
-	};
 
 	template<template<typename...> class List>
 	struct cartesian_product<List<>>
@@ -68,12 +18,43 @@ namespace meta
 		using type = List<>;
 	};
 
-	template<>
-	struct cartesian_product<>
+	template<template<typename...> class List, typename... Items>
+	struct cartesian_product<List<Items...>>
 	{
-		// sadly we don't know the list type here, so we return void
-		// and compensate this 'wrong' type by using if_ (see above)
-		using type = void;
+		template<typename T>
+		struct make_list
+		{
+			using type = List<T>;
+		};
+
+		using type = typename map<make_list, List<Items...> >::type;
+	};
+
+	template<template<typename...> class List, typename... Items, typename... OtherLists>
+	struct cartesian_product<List<Items...>, OtherLists...>
+	{
+		/// prepends Item to each element of _List
+		template<typename Item, typename _List1>
+		struct prepend_to_all
+		{
+			template<typename _List2>
+			struct prepend
+			{
+				using type = typename meta::prepend<Item, _List2>::type;
+			};
+
+			using type = typename meta::map<prepend, _List1>::type;
+		};
+
+		using sub_product = typename cartesian_product<OtherLists...>::type;
+
+		template<typename Item>
+		struct append_combinations
+		{
+			using type = typename prepend_to_all<Item, sub_product>::type;
+		};
+
+		using type = typename meta::foldr<meta::concat, List<>, typename map<append_combinations, List<Items...>>::type>::type;
 	};
 
 }
---
+++ test/test_cartesian_product.cpp
@@ -0,0 +1,55 @@
+#define BOOST_TEST_MODULE cartesian_product
+#include <boost/test/included/unit_test.hpp>
+
+#include <type_traits>
+#include <cartesian_product.hpp>
+
+template<typename...> struct List;
+
+BOOST_AUTO_TEST_CASE(empty_list)
+{
+	BOOST_CHECK( (std::is_same<typename meta::cartesian_product<List<>>::type, List<>>::value == true) );
+}
+
+BOOST_AUTO_TEST_CASE(one_list)
+{
+	BOOST_CHECK( (
+		std::is_same<
+			typename meta::cartesian_product<
+				List<int,float,bool>
+			>::type,
+			List<
+				List<int>,
+				List<float>,
+				List<bool>
+			>
+		>::value == true) );
+}
+
+template<typename> struct probe;
+
+BOOST_AUTO_TEST_CASE(two_lists)
+{
+	struct type1 {};
+	struct type2 {};
+	struct type3 {};
+
+	BOOST_CHECK( (
+		std::is_same<
+			typename meta::cartesian_product<
+				List<type1, type2, type3>,
+				List<type1, type2, type3>
+			>::type,
+			List<
+				List<type1, type1>,
+				List<type1, type2>,
+				List<type1, type3>,
+				List<type2, type1>,
+				List<type2, type2>,
+				List<type2, type3>,
+				List<type3, type1>,
+				List<type3, type2>,
+				List<type3, type3>
+			>
+		>::value == true) );
+}
